7a681b43b726fd27a90f2963d3546088fc81300b
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-08-13 17:21:34 +0200

    Added comments

--- src/main/scala/model/shape/Shape.scala
+++ src/main/scala/model/shape/Shape.scala
@@ -3,10 +3,23 @@ package model.shape
 
 import util.Position
 
+/**
+ * A Shape is an enclosed area where some point's membership can be verified.
+ */
 sealed trait Shape:
 
+  /**
+   * Checks if the point belongs (is internal) to the shape.
+   * @param pos the position to check
+   * @return
+   */
   def belongs(pos: Position): Boolean
 
+/**
+ * A class representing a Circle with radius and center.
+ * @param center the center of the circle
+ * @param radius the radius of the circle
+ */
 case class Circle(center: Position, radius: Double) extends Shape:
 
   def belongs(pos: Position): Boolean =
