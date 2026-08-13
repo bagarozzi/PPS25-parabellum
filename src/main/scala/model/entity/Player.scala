ef21aab37ce3bd87a294674d77ce59e39910f417
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-08-13 16:14:02 +0200

    Implemented Player interface

--- src/main/scala/model/entity/Player.scala
+++ src/main/scala/model/entity/Player.scala
@@ -1,5 +1,14 @@
 package it.unibo.parabellum
 package model.entity
 
+import util.Position
+
+import it.unibo.parabellum.model.shape.Shape
+
 trait Player extends Figure:
   val name: String
+  val pos: Position
+
+class PlayerImpl(val name: String, val pos: Position, val shape: Shape) extends Player:
+
+  override def belongs(pos: Position): Boolean = shape.belongs(pos)
