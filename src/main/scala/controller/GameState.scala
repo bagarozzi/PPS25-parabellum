4cb70e2f9ec15309fa9646139bc243d97b1485ee
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-08-13 15:28:16 +0200

    Added blank implementation of GameState

--- src/main/scala/controller/GameState.scala
+++ src/main/scala/controller/GameState.scala
@@ -1,4 +1,22 @@
 package it.unibo.parabellum
 package controller
 
-class GameState
+import model.entity.Player
+
+import it.unibo.parabellum.model.function.Projectile
+
+/**
+ * Represents the state of the game in a certain instant in time.
+ * @param players the set of players currently in the game
+ * @param projectiles the projectile that are being fired
+ */
+class GameState(val players: Set[Player], val projectiles: Set[Projectile])
+
+object GameState:
+
+  /**
+   * Updates the game state, returning a new state.
+   * @param g the game state to update
+   * @return the new game state
+   */
+  def update(g: GameState): GameState = ???
