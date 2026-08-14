a6a2b5a746290d4dd5ef4cff14b51bf0fbc4dbc0
Luca Venturini <luca.venturini14@studio.unibo.it>
2026-08-14 17:39:37 +0200

    small fixes

--- src/main/scala/model/CollisionDetector.scala
+++ src/main/scala/model/CollisionDetector.scala
@@ -3,12 +3,12 @@ package model
 
 import model.entity.{Entity, Player}
 
-import it.unibo.parabellum.controller.GameState
-import it.unibo.parabellum.model.function.Projectile
+import controller.GameState
+import model.function.Projectile
 
 object CollisionDetector
   def detectCollision(gs: GameState): Unit = {
     val offendedPlayer: Option[Player] = gs.players.find(p => !p.equals(gs.currentTurn))
-    if(gs.projectiles.isDefined && offendedPlayer.isDefined && offendedPlayer.get.belongs(gs.projectiles.get.position))
+    if gs.projectiles.isDefined && offendedPlayer.isDefined && offendedPlayer.get.belongs(gs.projectiles.get.position) then
       offendedPlayer.get.kill()
   }
