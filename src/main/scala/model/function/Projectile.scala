69010e783ac409b89652d7fa9b8443d7f99601dd
Luca Venturini <luca.venturini14@studio.unibo.it>
2026-08-14 17:36:45 +0200

    implementation of projectile and basic projectile

--- src/main/scala/model/function/Projectile.scala
+++ src/main/scala/model/function/Projectile.scala
@@ -1,4 +1,25 @@
 package it.unibo.parabellum
 package model.function
 
-trait Projectile
+import util.Position
+
+import it.unibo.parabellum.model.{ImpactEffect, Trajectory}
+
+
+trait Projectile:
+    def position: Position
+    def previousPosition: Position
+    def update(dt: Double): Projectile
+    def impactEffect: ImpactEffect
+    def vel: Double = 0.1
+
+case class BasicProjectile(
+  position: Position,
+  previousPosition: Position,
+  trajectory: Trajectory,
+  impactEffect: ImpactEffect
+) extends Projectile:
+
+  override def update(dt: Double): Projectile =
+    val newX = previousPosition.x + vel * dt
+    BasicProjectile(trajectory.compute(newX), position, trajectory, impactEffect)
