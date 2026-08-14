191ca4a5500a4e359c923ae17f428407b3464846
Luca Venturini <luca.venturini14@studio.unibo.it>
2026-08-14 17:35:05 +0200

    implementation of shape added method sample and BoundingBound for render and finished the Circle implementation

--- src/main/scala/model/shape/Shape.scala
+++ src/main/scala/model/shape/Shape.scala
@@ -6,25 +6,50 @@ import util.Position
 /**
  * A Shape is an enclosed area where some point's membership can be verified.
  */
+case class BoundingBound(minX: Double, maxX: Double, minY: Double, maxY:Double)
+
 sealed trait Shape:
+
+  def function: Position => Boolean
+
+  def bounds: BoundingBound
 
   /**
    * Checks if the point belongs (is internal) to the shape.
+   *
    * @param pos the position to check
    * @return
    */
-  def belongs(pos: Position): Boolean
+  final def belongs(pos: Position): Boolean =
+    function(pos)
+
+  final def sample(step: Double): Seq[Position] =
+    for
+      i <- 0 to ((bounds.maxX - bounds.minX) / step).toInt
+      j <- 0 to ((bounds.maxY - bounds.minY) / step).toInt
+      x = bounds.minX + i * step
+      y = bounds.minY + j * step
+      p = Position(x, y)
+      if belongs(p)
+    yield p
+      
 
 /**
  * A class representing a Circle with radius and center.
  * @param center the center of the circle
  * @param radius the radius of the circle
  */
-case class Circle(center: Position, radius: Double) extends Shape:
+case class Circle(
+                   center: Position,
+                   radius: Double
+                 ) extends Shape:
 
-  def belongs(pos: Position): Boolean =
-    val dx = pos.x - center.x
-    val dy = pos.y - center.y
-    (dx * dx + dy * dy) <= (radius * radius)
+  override val function: Position => Boolean =
+    p =>
+      val dx = p.x - center.x
+      val dy = p.y - center.y
 
+      dx * dx + dy * dy <= radius * radius
+
+  override val bounds: BoundingBound = BoundingBound(center.x - radius, center.x + radius, center.y - radius, center.y + radius)
 
