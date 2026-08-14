7ea984d401d356a1f2c6441f6b62a28a4c74b856
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-08-14 10:24:42 +0200

    Created init method for the Game State

--- src/main/scala/controller/GameState.scala
+++ src/main/scala/controller/GameState.scala
@@ -4,13 +4,15 @@ package controller
 import model.entity.Player
 
 import it.unibo.parabellum.model.function.Projectile
+import it.unibo.parabellum.util.Position
+import it.unibo.parabellum.model.entity.Player.initPlayer
 
 /**
  * Represents the state of the game in a certain instant in time.
  * @param players the set of players currently in the game
  * @param projectiles the projectile that are being fired
  */
-class GameState(val players: Set[Player], val projectiles: Set[Projectile])
+class GameState(val players: Set[Player], val projectiles: Option[Projectile], val currentTurn: Player)
 
 object GameState:
 
@@ -21,4 +23,9 @@ object GameState:
    */
   def update(g: GameState): GameState = ???
 
-  def init(): GameState = GameState(Set(), Set())
+  def init(): GameState =
+    val players = Set(initPlayer("player1", Position(-7.5, 0.0)), initPlayer("player2", Position(7.5, 0.0)))
+    GameState(
+    players,
+    None,
+    players.head)
