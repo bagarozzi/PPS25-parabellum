f5cb478d07c28bfd682fae1d506771c117193853
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-08-14 14:22:38 +0200

    Created and passed game state

--- src/main/scala/Parabellum.scala
+++ src/main/scala/Parabellum.scala
@@ -1,10 +1,10 @@
 package it.unibo.parabellum
 
-import controller.Engine
+import controller.{Engine, GameState}
 
 object Parabellum:
 
   @main def main(): Unit =
     // Create: View, GameState and pass it to the main loop
     given targetFPS: Int = 60
-    Engine.run(???)
+    Engine.run(GameState.init())
