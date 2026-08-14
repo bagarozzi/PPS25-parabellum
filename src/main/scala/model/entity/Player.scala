55cfbecee0b0c75d774443a721a356fbe0844429
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-08-14 10:24:32 +0200

    Created init method for Player

--- src/main/scala/model/entity/Player.scala
+++ src/main/scala/model/entity/Player.scala
@@ -2,8 +2,7 @@ package it.unibo.parabellum
 package model.entity
 
 import util.Position
-
-import model.shape.Shape
+import model.shape.{Circle, Shape}
 
 trait Player extends Figure:
   val name: String
@@ -11,3 +10,9 @@ trait Player extends Figure:
 class PlayerImpl(val name: String, val pos: Position, val shape: Shape) extends Player:
 
   override def belongs(pos: Position): Boolean = shape.belongs(pos)
+
+object Player:
+
+  private val PLAYER_RADIUS: Double = 0.15
+
+  def initPlayer(name: String, pos: Position): Player = PlayerImpl(name, pos, Circle(pos, PLAYER_RADIUS))
