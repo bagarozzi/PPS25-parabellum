2d44b286889b9136dc07282feaebfd4340fe1b1b
Luca Venturini <luca.venturini14@studio.unibo.it>
2026-08-15 17:06:41 +0200

    added state

--- src/main/scala/model/entity/Player.scala
+++ src/main/scala/model/entity/Player.scala
@@ -4,10 +4,17 @@ package model.entity
 import util.Position
 import model.shape.{Circle, Shape}
 
+import it.unibo.parabellum.model.Trajectory
+
 trait Player extends Figure:
   val name: String
+  def state: State
   def kill(): Unit
+  def shoot(trajectory: Trajectory): Unit
 
+enum State:
+  case alive, dead
+  
 class PlayerImpl(val name: String, val pos: Position, val shape: Shape) extends Player:
 
   override def belongs(pos: Position): Boolean = shape.belongs(pos)
@@ -21,3 +28,5 @@ object Player:
   private val PLAYER_RADIUS: Double = 0.15
 
   def initPlayer(name: String, pos: Position): Player = PlayerImpl(name, pos, Circle(pos, PLAYER_RADIUS))
+  
+  def shoot(trajectory: Trajectory): Unit = ???
