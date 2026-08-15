d57fc786448df16b436321578e7c5bdb0d3034d8
Luca Venturini <luca.venturini14@studio.unibo.it>
2026-08-15 18:28:53 +0200

    added buffer for pending projectile

--- src/main/scala/controller/GameState.scala
+++ src/main/scala/controller/GameState.scala
@@ -5,8 +5,9 @@ import model.entity.Player
 
 import it.unibo.parabellum.util.Position
 import it.unibo.parabellum.model.entity.Player.initPlayer
-import model.function.Projectile
+import model.function.{BasicProjectile, Projectile}
 
+import it.unibo.parabellum.model.{ImpactEffect, Trajectory}
 import it.unibo.parabellum.model.entity.State.dead
 
 /**
@@ -15,21 +16,25 @@ import it.unibo.parabellum.model.entity.State.dead
  * @param projectiles the projectile that are being fired
  */
 class GameState(val players: Set[Player], val projectiles: Option[Projectile], val currentTurn: Player)
-
 object GameState:
-
+  var pendingTrajectory: Option[Trajectory] = None
   /**
    * Updates the game state, returning a new state.
    * @param g the game state to update
    * @return the new game state
    */
-  def update(g: GameState, dt: Double): GameState = {
+  def update(g: GameState, dt: Double): GameState = 
+    if(g.projectiles.isEmpty && pendingTrajectory.isDefined) then 
+      val newProjectile = BasicProjectile(g.currentTurn.pos, pendingTrajectory.get, ImpactEffect(), 1)
     GameState(g.players.diff(g.players.filter(p => p.state == dead).toSet),
       g.projectiles.map(p => p.update(dt)),
       g.currentTurn)
-  }
+    
   
-  
+
+  def addProjectile(trajectory: Trajectory): Unit =
+    pendingTrajectory = Some(trajectory)
+    
 
 
   def init(): GameState =
