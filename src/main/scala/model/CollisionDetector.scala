7d16c6cad3f57746c223038455e75de7e5c67eec
Luca Venturini <luca.venturini14@studio.unibo.it>
2026-08-15 17:39:52 +0200

    added chech collisions for borders

--- src/main/scala/model/CollisionDetector.scala
+++ src/main/scala/model/CollisionDetector.scala
@@ -2,13 +2,22 @@ package it.unibo.parabellum
 package model
 
 import model.entity.{Entity, Player}
-
 import controller.GameState
 import model.function.Projectile
+import util.Position
+
+import it.unibo.parabellum.util.BoundingBox
 
 object CollisionDetector
-  def detectCollision(gs: GameState): Unit = {
-    val offendedPlayer: Option[Player] = gs.players.find(p => !p.equals(gs.currentTurn))
-    if gs.projectiles.isDefined && offendedPlayer.isDefined && offendedPlayer.get.belongs(gs.projectiles.get.position) then
-      offendedPlayer.get.kill()
+  val border: BoundingBox = BoundingBox(-15, 15, -25, 25)
+  def detectCollision(gs: GameState): Option[Projectile] = {
+    val offendedPlayer: Player = gs.players.find(p => !p.equals(gs.currentTurn)).get
+    if gs.projectiles.isDefined then 
+      val projectile = gs.projectiles.get
+      if border.checkBoundary(projectile.position) then
+        return None
+      if(offendedPlayer.belongs(projectile.position)) then
+        offendedPlayer.kill()
+    gs.projectiles
   }
+    
