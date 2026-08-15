655db971633099a0a19d3226aeba898a00b61088
Luca Venturini <luca.venturini14@studio.unibo.it>
2026-08-15 17:06:54 +0200

    added direction

--- src/main/scala/model/function/Projectile.scala
+++ src/main/scala/model/function/Projectile.scala
@@ -8,18 +8,18 @@ import it.unibo.parabellum.model.{ImpactEffect, Trajectory}
 
 trait Projectile:
     def position: Position
-    def previousPosition: Position
     def update(dt: Double): Projectile
     def impactEffect: ImpactEffect
     def vel: Double = 0.1
 
 case class BasicProjectile(
   position: Position,
-  previousPosition: Position,
   trajectory: Trajectory,
-  impactEffect: ImpactEffect
+  impactEffect: ImpactEffect,
+  direction: Int
 ) extends Projectile:
 
   override def update(dt: Double): Projectile =
-    val newX = previousPosition.x + vel * dt
-    BasicProjectile(trajectory.compute(newX), position, trajectory, impactEffect)
+    val newX = position.x + (vel * dt * direction)
+    
+    BasicProjectile(trajectory.compute(newX), trajectory, impactEffect, direction)
