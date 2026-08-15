bc8ca1575cc22d3dd5206c8470c690248fe1e245
Luca Venturini <luca.venturini14@studio.unibo.it>
2026-08-15 18:29:06 +0200

    refactor

--- src/main/scala/model/CollisionDetector.scala
+++ src/main/scala/model/CollisionDetector.scala
@@ -8,16 +8,28 @@ import util.Position
 
 import it.unibo.parabellum.util.BoundingBox
 
-object CollisionDetector
-  val border: BoundingBox = BoundingBox(-15, 15, -25, 25)
-  def detectCollision(gs: GameState): Option[Projectile] = {
-    val offendedPlayer: Player = gs.players.find(p => !p.equals(gs.currentTurn)).get
+object CollisionDetector:
+  given border: BoundingBox = BoundingBox(-15, 15, -25, 25)
+
+def checkCollisionWithBorders(projectile: Projectile)(using border: BoundingBox): Option[Projectile] = {
+  if border.checkBoundary(projectile.position) then
+    return None
+  Some(projectile)
+}
+
+def CheckCollisionWithPlayer(offendedPlayers: List[Player], projectile: Projectile): Unit = {
+  offendedPlayers.filter(p => p.belongs(projectile.position)).foreach(p => p.kill())
+}
+
+def detectCollision(gs: GameState)(using border: BoundingBox): Option[Projectile] = {
+    val offendedPlayer: List[Player] = List(gs.players.find(p => !p.equals(gs.currentTurn)).get)
     if gs.projectiles.isDefined then 
       val projectile = gs.projectiles.get
-      if border.checkBoundary(projectile.position) then
-        return None
-      if(offendedPlayer.belongs(projectile.position)) then
-        offendedPlayer.kill()
+      checkCollisionWithBorders(projectile) match
+        case None => return None
+        case toReturn => return toReturn
+      
+      CheckCollisionWithPlayer(offendedPlayer, projectile)
     gs.projectiles
   }
     
