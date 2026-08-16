47c8b4e8cfcea69f1bf193a77c0584f9337e99ad
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-08-16 17:07:33 +0200

    Added ScalaFX AnimationTimer as GameLoop

--- src/main/scala/controller/Controller.scala
+++ src/main/scala/controller/Controller.scala
@@ -3,24 +3,44 @@ package controller
 
 import view.View
 
+import it.unibo.parabellum.model.function.Projectile
+import scalafx.animation.AnimationTimer
+
 trait Controller:
 
     def startGame(): Unit
 
-    def addBullet(): Unit
+    def addProjectile(angularCoefficient: Double): Unit
 
     def updateView(g: GameState)(using view: View): Unit
 
 object GameController extends Controller:
 
     private var gameState: Option[GameState] = None
+    private var gameLoop: Option[AnimationTimer] = None
+    private var lastTime: Long = System.nanoTime()
+
+    import Parabellum.given
 
     def startGame(): Unit =
-        given targetFPS: Int = 60
+        //given targetFPS: Int = 60
         gameState = Some(GameState.init())
-        Engine.run(gameState.get)
 
-    def addBullet(): Unit = ???
+        lastTime = System.nanoTime()
+        val timer = AnimationTimer {
+            time =>
+                gameState = Some(GameState.update(gameState.get, (time - lastTime)/1_000_000))
+                lastTime = time
+                updateView(gameState.get)
+        }
+        timer.start()
+        gameLoop = Some(timer)
+    //Engine.run(gameState.get)
+
+    def addProjectile(angularCoefficient: Double): Unit = gameState match
+        case Some(g) => GameState.addProjectile(Projectile.parseStraightLine(g.currentTurn.pos, angularCoefficient))
+        case None =>
+
 
     def updateView(g: GameState)(using view: View): Unit =
         view.render(g)
