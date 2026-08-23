0141eb7a5be583b5ecbfce7833a56c030250d73a
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-08-23 17:06:24 +0200

    Solved some warnings

--- src/main/scala/view/ControlPanelView.scala
+++ src/main/scala/view/ControlPanelView.scala
@@ -1,18 +1,19 @@
 package it.unibo.parabellum.view
 
-import scalafx.scene.layout.{HBox, Priority, Region}
+import scalafx.scene.layout.{HBox, Region}
 import scalafx.geometry.Insets
 import scalafx.scene.control.{Button, Label, TextField}
 import scalafx.scene.paint.Color.*
 import scalafx.Includes.*
+import scalafx.event.ActionEvent
 import scalafx.scene.layout.Priority.Always
 
 /**
  * Pannello di controllo in basso con gli input per il tiro.
  *
- * @param onSpara Funzione di callback eseguita quando si preme "Spara"
+ * @param onShootLeft Funzione di callback eseguita quando si preme "Spara"
  */
-class ControlPanelView(leftPlayerName: String, rightPlayerName: String, onShootLeft: (String) => Unit, onShootRight: String => Unit) extends HBox:
+class ControlPanelView(leftPlayerName: String, rightPlayerName: String, onShootLeft: String => Unit, onShootRight: String => Unit) extends HBox:
 
   // Stile del pannello: spaziatura interna e colore di sfondo scuro
   padding = Insets(15)
@@ -29,7 +30,9 @@ class ControlPanelView(leftPlayerName: String, rightPlayerName: String, onShootL
     prefWidth = 80
   private val leftButton = new Button("Shoot"):
     style = "-fx-cursor: hand; -fx-font-weight: bold;"
-    onAction = _ => onShootLeft(leftField.text())
+    onAction = (ae: ActionEvent) => {
+     onShootLeft(leftField.text.value)
+    }
 
   private val rightLabel = new Label(rightPlayerName):
     textFill = White
@@ -38,7 +41,7 @@ class ControlPanelView(leftPlayerName: String, rightPlayerName: String, onShootL
     prefWidth = 80
   private val rightButton = new Button("Shoot"):
     style = "-fx-cursor: hand; -fx-font-weight: bold;"
-    onAction = _ => onShootRight(leftField.text())
+    onAction = () => onShootRight(leftField.text.value)
 
 
   children = List(leftLabel, leftField, leftButton,
