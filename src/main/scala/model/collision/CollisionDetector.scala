5b7ba4de679924a88e14152ccb7988812b3db249
Luca Venturini <luca.venturini14@studio.unibo.it>
2026-08-23 12:21:17 +0200

    added obstacles in collision

--- src/main/scala/model/collision/CollisionDetector.scala
+++ src/main/scala/model/collision/CollisionDetector.scala
@@ -2,26 +2,29 @@ package it.unibo.parabellum
 package model.collision
 
 import controller.GameState
-import model.entity.{Entity, Player}
+import model.entity.{Entity, Figure, Obstacle, Player}
 import model.function.Projectile
 import util.{BoundingBox, Position}
+import model.entity.Soldier
 
 object CollisionDetector:
   val border: BoundingBox = BoundingBox(-25, 25, -15, 15)
   given BoundingBox = border
 
-  def checkCollisionWithBorders(projectile: Projectile): Option[Projectile] = 
+  private def checkCollisionWithBorders(projectile: Projectile): Option[Projectile] = 
     if !CollisionDetector.border.checkBoundary(projectile.pos()) then
       return None
     Some(projectile)
 
 
-  def CheckCollisionWithPlayer(offendedPlayers: Set[Player], projectile: Projectile): Unit = 
-    offendedPlayers.filter(p => p.belongs(projectile.pos())).foreach(p => p.kill())
-  
+  private def CheckCollisionWithFigure(entities: Set[Figure], projectile: Projectile): Unit = 
+    entities.filter(f => f.belongs(projectile.pos())).foreach {
+      case soldier: Soldier => soldier.kill()
+      case obstacle: Obstacle => ???
+    }
 
-  def detectCollision(projectile: Projectile, offendedPlayers: Set[Player]): Option[Projectile] = 
-    CheckCollisionWithPlayer(offendedPlayers, projectile)
+  def detectCollision(projectile: Projectile, entities: Set[Figure]): Option[Projectile] = 
+    CheckCollisionWithFigure(entities, projectile)
     checkCollisionWithBorders(projectile) match
       case None => return None
       case toReturn => return toReturn
