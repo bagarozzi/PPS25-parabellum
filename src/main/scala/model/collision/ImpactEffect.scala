6eca43866f4cbbf404d80fc7925cb4f5eb08d34e
Luca Venturini <luca.venturini14@studio.unibo.it>
2026-08-24 16:44:08 +0200

    implementation of ImpactEffect with Impact trait and ImpactEvent

--- src/main/scala/model/collision/ImpactEffect.scala
+++ src/main/scala/model/collision/ImpactEffect.scala
@@ -4,23 +4,38 @@ package model.collision
 import model.entity.{Figure, Obstacle, Soldier}
 
 import it.unibo.parabellum.controller.GameState
-import it.unibo.parabellum.model.shape.{Circle, Difference}
-import it.unibo.parabellum.util.Position
+import it.unibo.parabellum.model.shape.{Circle, Difference, Shape}
+import it.unibo.parabellum.util.Position 
 
 trait ImpactEffect:
-  def applyEffect(impact: Impact, gs: GameState): ImpactEvent
-  
-trait ImpactEvent:
-  def resolve(gameState: GameState): GameState
+  def applyEffect(impact: Impact): Set[ImpactEvent]
+
+sealed trait ImpactEvent
+
+case class KillSoldier(soldier: Soldier) extends ImpactEvent
+
+case class DamageObstacle(obstacle: Obstacle, hole: Shape) extends ImpactEvent
+
+case object DestroyProjectile extends ImpactEvent
 
 object ImpactEffect:
-  def normalImpactEffect(): ImpactEffect =
-    (impact: Impact, gs: GameState) => impact.hit match 
-      case obs: Obstacle => (gs: GameState) => GameState(gs.manager, gs.obstacles - obs + Obstacle(obs.pos, Difference(obs.shape, Set(Circle(impact.pos, 0.5)))), None, gs.pendingFunction)
-      case sld: Soldier => ???
+  def normalImpactEffect(): ImpactEffect = 
+    {
+      case FigureImpact(pos, obs: Obstacle) => Set(DamageObstacle(obs, Circle(pos, 0.5)), DestroyProjectile)
+      case FigureImpact(pos, sld: Soldier) => Set(KillSoldier(sld))
+      case BorderImpact() => Set(DestroyProjectile)
+      case FigureImpact(Position(_, _), _) => Set()
+    }
 
-case class Impact(
-                 val pos: Position,
-                 val hit: Figure
-                 )
+sealed trait Impact
 
+case class FigureImpact(
+                         pos: Position,
+                         figure: Figure
+                       ) extends Impact
+
+case class BorderImpact() extends Impact
+
+
+//GameState(gs.manager, gs.obstacles - obs + Obstacle(obs.pos, Difference(obs.shape, Set(Circle(impact.pos, 0.5)))), None, gs.pendingFunction)
+//(gs: GameState) => GameState(gs.manager.eliminateDeadSoldier(sld), gs.obstacles, gs.projectiles, gs.pendingFunction)
