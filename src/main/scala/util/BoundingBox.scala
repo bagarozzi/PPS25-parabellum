e56686b79836bf775c11e65a20b0d0d1dfc11c28
Federico Bagattoni <federicobagattoni61@gmail.com>
2026-08-25 15:54:12 +0200

    Added method to calculate aspect ratio

--- src/main/scala/util/BoundingBox.scala
+++ src/main/scala/util/BoundingBox.scala
@@ -5,3 +5,4 @@ case class BoundingBox(x0: Double, x1: Double, y0: Double, y1: Double):
     def hsize: Double = Math.abs(x0) + Math.abs(x1)
     def vsize: Double = Math.abs(y0) + Math.abs(y1)
     def checkBoundary(pos: Position): Boolean = pos.x >= x0 && pos.x <= x1 && pos.y >= y0 && pos.y <= y1
+    def ratio: Double = hsize / vsize
