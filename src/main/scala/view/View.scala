dd101c6bb783cac70e26af649eb8ebf6b0b119c7
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-08-31 18:04:51 +0200

    Added given

--- src/main/scala/view/View.scala
+++ src/main/scala/view/View.scala
@@ -1,11 +1,12 @@
 package it.unibo.parabellum.view
 
 import it.unibo.parabellum.controller.GameState
+import it.unibo.parabellum.util.BoundingBox
 
 /**
  * Interfaccia della View che l'Engine utilizzerà per aggiornare la grafica.
  */
 trait View:
-  def render(state: GameState): Unit
+  def render(state: GameState)(using border: BoundingBox): Unit
   
   def start(): Unit
