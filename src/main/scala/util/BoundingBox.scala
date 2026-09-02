830c13d878d30f1d88a58bf2dfcef377fe88f40e
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-09-02 19:43:05 +0200

    Corrected checking error

--- src/main/scala/util/BoundingBox.scala
+++ src/main/scala/util/BoundingBox.scala
@@ -5,6 +5,6 @@ case class BoundingBox(x0: Double, x1: Double, y0: Double, y1: Double):
     def hsize: Double = Math.abs(x0) + Math.abs(x1)
     def vsize: Double = Math.abs(y0) + Math.abs(y1)
     def checkBoundary(pos: Position): Boolean = pos.x >= x0 && pos.x <= x1 && pos.y >= y0 && pos.y <= y1
-    def checkSideViolation(pos: Position): Boolean = !checkBoundary(pos) && (pos.x >= x0 || pos.x <= x1)
-    def checkTopBottomViolation(pos: Position): Boolean = !checkBoundary(pos) && (pos.y >= y0 || pos.y <= y1)
+    def checkSideViolation(pos: Position): Boolean = !checkBoundary(pos) && (pos.x <= x0 || pos.x >= x1)
+    def checkTopBottomViolation(pos: Position): Boolean = !checkBoundary(pos) && (pos.y <= y0 || pos.y >= y1)
     def ratio: Double = hsize / vsize
