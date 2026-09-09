8afed80c6e73eb731f670aef4b5ca92b4183520f
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-09-09 16:34:18 +0200

    Refactor: removed unnecessary method

--- src/main/scala/model/function/Projectile.scala
+++ src/main/scala/model/function/Projectile.scala
@@ -39,12 +39,9 @@ private case class ProjectileI(trajectory: Trajectory, effect: ImpactEffect) ext
 object Projectile:
 
   def createProjectile(startingPosition: Position, function: Function, direction: Int, impactEffect: ImpactEffect, powerUp: Option[PowerUp]): Projectile =
-    createModifiedProjectile(powerUp.fold(impactEffect)(_.impactEffect), startingPosition, direction, powerUp.fold(function)(_.trajectoryDistortion(function)))
+    ProjectileI(
+        Trajectory.create(startingPosition, powerUp.fold(function)(_.trajectoryDistortion(function)), if direction > 0 then Direction.Positive else Direction.Negative),
+        powerUp.fold(impactEffect)(_.impactEffect)
+    )
 
   def fromSoldier(p: Player, s: Soldier, function: Function): Projectile = createProjectile(s.pos, function, s.facingDirection, p.impactEffect, p.getPowerUp)
-
-  private def createModifiedProjectile(impactEffect: ImpactEffect, startingPosition: Position, direction: Int, func: Function): Projectile =
-    ProjectileI(
-      Trajectory.create(startingPosition, func, if direction > 0 then Direction.Positive else Direction.Negative),
-        impactEffect
-    )
