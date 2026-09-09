4c135c0d0b21341bf12e784044222a6654d93944
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-09-09 16:51:51 +0200

    Added comments

--- src/main/scala/model/function/Projectile.scala
+++ src/main/scala/model/function/Projectile.scala
@@ -38,10 +38,27 @@ private case class ProjectileI(trajectory: Trajectory, effect: ImpactEffect) ext
 
 object Projectile:
 
+    /**
+     * Creates a [[Projectile]] from the passed arguments
+     * @param startingPosition the position where it will start moving from
+     * @param function the mathematical [[Function]] that it will follow
+     * @param direction the direction of travel of the [[Function]]
+     * @param impactEffect the [[ImpactEffect]] that the Projectile will have
+     * @param powerUp the eventual [[PowerUp]] that is to be applied to this Projectile
+     * @return a new [[Projectile]]
+     */
     def createProjectile(startingPosition: Position, function: Function, direction: Direction, impactEffect: ImpactEffect, powerUp: Option[PowerUp]): Projectile =
         ProjectileI(
             Trajectory.create(startingPosition, powerUp.fold(function)(_.trajectoryDistortion(function)), direction),
             powerUp.fold(impactEffect)(_.impactEffect)
         )
 
+    /**
+     * Convenience method that builds a [[Projectile]] using the
+     * data from a [[Player]] and it's [[Soldier]].
+     * @param p the [[Player]] shooting the Projectile
+     * @param s the [[Soldier]] where the Projectile will start from
+     * @param function the [[Function]] that the Projectile will follow
+     * @return a new [[Projectile]]
+     */
     def fromSoldier(p: Player, s: Soldier, function: Function): Projectile = createProjectile(s.pos, function, s.facingDirection, p.impactEffect, p.getPowerUp)
